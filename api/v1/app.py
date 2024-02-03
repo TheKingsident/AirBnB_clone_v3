54084eabebaf1ebe7bed5087b8306c7c60356c2a
TheKingsident <kingsident@gmail.com>
2024-02-04 00:04:31 +0100

    Updated to include 404 error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """API to return status"""
 from os import getenv
 from flask import Flask
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -16,8 +17,11 @@ def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"})
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
