ec0439c8830af7b382bfd0f411ec0923579c132a
TheKingsident <kingsident@gmail.com>
2024-02-03 23:30:26 +0100

    Updated for new route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """Returns the API status"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -12,3 +13,17 @@ def status():
             "status": "OK"
         }
     )
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Retrieves the number of each object by type."""
+    obj_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Places"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+
+    return jsonify(obj_counts)
