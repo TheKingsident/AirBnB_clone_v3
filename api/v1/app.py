c4d744a26113fc4ab6d396957cb3fb0c8f9e8187
TheKingsident <kingsident@gmail.com>
2024-02-05 17:26:59 +0100

    Updated a file

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,14 @@
 from os import getenv
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
